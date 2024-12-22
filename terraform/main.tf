a77a1802c6d1d36255dd602cd226bfa5f7782763
anouuar <anouartm2@gmail.com>
2024-12-22 22:19:17 +0100

    ajout de terraform v1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 provider "docker" {}
 
 resource "docker_image" "mysql" {
@@ -16,25 +26,5 @@ resource "docker_container" "mysql" {
     "MYSQL_DATABASE=akaunting",
     "MYSQL_USER=akaunting_user",
     "MYSQL_PASSWORD=akaunting_password"
-  ]
-}
-
-resource "docker_image" "akaunting" {
-  name = "akaunting/akaunting:latest"
-}
-
-resource "docker_container" "akaunting" {
-  image = docker_image.akaunting.latest
-  name  = "terraform-akaunting"
-  ports {
-    internal = 80
-    external = 8000
-  }
-  env = [
-    "DB_HOST=terraform-mysql",
-    "DB_PORT=3306",
-    "DB_DATABASE=akaunting",
-    "DB_USERNAME=akaunting_user",
-    "DB_PASSWORD=akaunting_password"
   ]
 }
